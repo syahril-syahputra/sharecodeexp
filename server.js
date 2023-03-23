c6671d67f4044a371eddd9e959ffbfc60e87f4ad
kdkchy <kdkchy@gmail.com>
2023-03-23 08:57:34 +0800

    new server script

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = 'production'
 const hostname = 'localhost'
 const port = 3000
 // when using middleware `hostname` and `port` must be provided below
