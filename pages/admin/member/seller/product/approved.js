a659f6a229b2f6275d1941ff0875cd2a258b1d9d
syahril syahputra <syahril.s@radx.sg>
2023-10-11 00:36:24 +0800

    finish upload for user

--- pages/admin/member/seller/product/approved.js
+++ pages/admin/member/seller/product/approved.js
@@ -11,84 +11,84 @@ import MiniSearchBar from "@/components/Shared/MiniSearchBar";
 import { toast } from 'react-toastify';
 import { toastOptions } from "@/lib/toastOptions"
 
-export default function MyProduct({session}) {
+export default function MyProduct({ session }) {
 
-  //data search
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
-  const [links, setLinks] = useState([])
-  const [metaData, setMetaData] = useState({
-    total: 0,
-    perPage: 0,
-    lastPage: 0
-  })
+    //data search
+    const [isLoading, setIsLoading] = useState(true)
+    const [data, setData] = useState([])
+    const [links, setLinks] = useState([])
+    const [metaData, setMetaData] = useState({
+        total: 0,
+        perPage: 0,
+        lastPage: 0
+    })
 
-  const [search, setSearch] = useState('')
-  async function searchData(searchParam='', page = 1) {
-    setSearch(searchParam)
-    setIsLoading(true);
-    const response = await axios.get(`/companyproduct?page=${page}&status=approved&search=${searchParam}`,
-      {
-        headers: {
-          "Authorization": `Bearer ${session.accessToken}`
-        }
-      }
-    )
-      .then((response) => {
-        let result = response.data.data;
-        setData(result.data);
-        setLinks(result.links);
-        setMetaData({
-          total: result.total,
-          perPage: result.per_page,
-          lastPage: result.last_page,
-          currentPage: result.current_page,
-          nextPage: result.next_page_url ? true : false,
-          prevPage: result.prev_page_url ? true : false
-        });
-      }).catch((error) => {
-        setData([]);
-        toast.error("Something went wrong. Cannot load component.", toastOptions);
-      }).finally(() => {
-        setIsLoading(false);
-      });
-  }
-  const setPage = (pageNumber) => {
-    searchData(search, pageNumber)
-  }
-  useEffect(() => {
-    searchData()
-  }, [])
-  const handleSearch = (searchResult) =>{
-    searchData(searchResult)
-  }
+    const [search, setSearch] = useState('')
+    async function searchData(searchParam = '', page = 1) {
+        setSearch(searchParam)
+        setIsLoading(true);
+        const response = await axios.get(`/companyproduct?page=${page}&status=approved&search=${searchParam}`,
+            {
+                headers: {
+                    "Authorization": `Bearer ${session.accessToken}`
+                }
+            }
+        )
+            .then((response) => {
+                let result = response.data.data;
+                setData(result.data);
+                setLinks(result.links);
+                setMetaData({
+                    total: result.total,
+                    perPage: result.per_page,
+                    lastPage: result.last_page,
+                    currentPage: result.current_page,
+                    nextPage: result.next_page_url ? true : false,
+                    prevPage: result.prev_page_url ? true : false
+                });
+            }).catch((error) => {
+                setData([]);
+                toast.error("Something went wrong. Cannot load component.", toastOptions);
+            }).finally(() => {
+                setIsLoading(false);
+            });
+    }
+    const setPage = (pageNumber) => {
+        searchData(search, pageNumber)
+    }
+    useEffect(() => {
+        searchData()
+    }, [])
+    const handleSearch = (searchResult) => {
+        searchData(searchResult)
+    }
 
-  return (
-    <>
-      <div className="mb-10">
-        <div className="mb-5 w-full lg:w-1/2">
-            <MiniSearchBar searchItem={handleSearch}/>
-        </div>
-        <ComponentList
-          title="Approved Product"
-          setPage={setPage}
-          isLoading={isLoading}
-          data={data}
-          links={links}
-          metaData={metaData}
-        ></ComponentList>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="mb-10">
+                <div className="mb-5 w-full lg:w-1/2">
+                    <MiniSearchBar searchItem={handleSearch} />
+                </div>
+                <ComponentList
+                    title="My Products"
+                    setPage={setPage}
+                    isLoading={isLoading}
+                    data={data}
+                    links={links}
+                    metaData={metaData}
+                ></ComponentList>
+            </div>
+        </>
+    );
 }
 
 MyProduct.layout = Admin;
 
 export async function getServerSideProps(context) {
-  const session = await getSession(context)
-  return {
-      props: {
-          session
-      }
-  }
+    const session = await getSession(context)
+    return {
+        props: {
+            session
+        }
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './layouts/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    content: [
+        './node_modules/flowbite-react/**/*.js',
+        './layouts/**/*.{js,ts,jsx,tsx}',
+        './app/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
 
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/react-tailwindcss-select/dist/index.esm.js',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      lineHeight: {
-        'home-letter': '10.278rem',
-        'paragraph-letter': '1.741rem',
-      },
-      letterSpacing: {
-        'home-spacing': '30%',
-      },
-      colors: {
-        'secondary-white': '#c7c7c7',
-        'sub-header': '#3730A3',
-        'top-navbar': '#03072B',
-        'footer-resources': '#3B82F6',
-      },
-      backgroundColor: {
-        'sub-content': '#DCDEEE',
-      },
+        // Or if using `src` directory:
+        './src/**/*.{js,ts,jsx,tsx}',
+        './node_modules/react-tailwindcss-select/dist/index.esm.js',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            lineHeight: {
+                'home-letter': '10.278rem',
+                'paragraph-letter': '1.741rem',
+            },
+            letterSpacing: {
+                'home-spacing': '30%',
+            },
+            colors: {
+                'secondary-white': '#c7c7c7',
+                'sub-header': '#3730A3',
+                'top-navbar': '#03072B',
+                'footer-resources': '#3B82F6',
+            },
+            backgroundColor: {
+                'sub-content': '#DCDEEE',
+            },
+        },
     },
-  },
-  plugins: [require('flowbite/plugin')],
+    plugins: [require('flowbite/plugin')],
 };
