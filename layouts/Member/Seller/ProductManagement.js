c179bfbfec54b2789f37043235af593fc1855cf5
syahril syahputra <syahril.s@radx.sg>
2023-11-16 14:21:26 +0800

    finish counter excel file

--- layouts/Member/Seller/ProductManagement.js
+++ layouts/Member/Seller/ProductManagement.js
@@ -6,7 +6,7 @@ import { Disclosure } from '@headlessui/react'
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
 import PrimaryBadges from '@/components/Interface/Badges/PrimaryBadges'
 
-export default function ProductManagement({ product }) {
+export default function ProductManagement({ product, excel }) {
   return (
     <li>
       <Disclosure as="div">
@@ -81,7 +81,7 @@ export default function ProductManagement({ product }) {
                   'rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700 flex justify-between'
                 )}
               >
-                Uploaded Excel File
+                Uploaded Excel File <PrimaryBadges title={excel.uploaded} />
               </Link>
               <Link
                 href="/admin/member/seller/product/add"
--- layouts/Superadmin/ProductManagement.js
+++ layouts/Superadmin/ProductManagement.js
@@ -7,7 +7,7 @@ import { Disclosure } from '@headlessui/react'
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
 import PrimaryBadges from '@/components/Interface/Badges/PrimaryBadges'
 
-export default function ComponentManagement({ product }) {
+export default function ComponentManagement({ product, excel }) {
   const publicDir = process.env.NEXT_PUBLIC_DIR
 
   return (
@@ -73,7 +73,7 @@ export default function ComponentManagement({ product }) {
                   'rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700 flex justify-between'
                 )}
               >
-                Uploaded Excel File
+                Uploaded Excel File <PrimaryBadges title={excel.uploaded} />
               </Link>
               <Link
                 href={publicDir + '/template/exepart_template.xlsx'}
