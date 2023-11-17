fbb0e5354a2db43d4e2c18c921d924f0456ab941
adly.ms@gmail.com <adly@venatronics.com>
2023-11-17 14:46:45 +0700

    [WEB DEVELOPMENT] order seller

--- pages/admin/member/seller/incoming-inquiry/inquiry-rejected.js
+++ pages/admin/member/seller/incoming-inquiry/inquiry-rejected.js
@@ -51,7 +51,8 @@ export default function InquiryRejected({ session }) {
           `&status=inquiry-rejected` +
           `&order_number=${orderNumberParam}` +
           `&manufacturer_part_number=${manufacturerPartNumberParam}` +
-          `&order_date=${orderDateParam}`
+          `&order_date=${orderDateParam}` +
+          `&action_required=${false}`
         : '/seller/order/list' +
           `?page=${page}` +
           `&status=inquiry-rejected` +
@@ -111,6 +112,10 @@ export default function InquiryRejected({ session }) {
     loadData()
   }, [])
 
+  useEffect(() => {
+    handleSearchData()
+  }, [stateActionRequired])
+
   return (
     <>
       <div className="mb-10">
@@ -156,12 +161,11 @@ export default function InquiryRejected({ session }) {
                   id="stateActionRequired"
                   onChange={(e) => {
                     setStateActionRequired(!stateActionRequired)
-                    loadData(stateActionRequired)
                   }}
                 />
                 <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                 <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                  Action Require{' '}
+                  Action Required{' '}
                 </span>
               </label>
             </div>
