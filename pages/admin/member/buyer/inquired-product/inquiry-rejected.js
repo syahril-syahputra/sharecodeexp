cf7d0ffc85b7558f131d3fc4b2691b32b372a2c0
adly.ms@gmail.com <adly@venatronics.com>
2023-11-17 14:31:12 +0700

    [WEB DEVELOPMEN ~ADLY] buyer order improve with action_required

--- pages/admin/member/buyer/inquired-product/inquiry-rejected.js
+++ pages/admin/member/buyer/inquired-product/inquiry-rejected.js
@@ -52,7 +52,8 @@ export default function InquiryRejected({ session }) {
           `&status=inquiry-rejected` +
           `&order_number=${orderNumberParam}` +
           `&manufacturer_part_number=${manufacturerPartNumberParam}` +
-          `&order_date=${orderDateParam}`
+          `&order_date=${orderDateParam}` +
+          `&action_required=${false}`
         : '/buyer/order/list' +
           `?page=${page}` +
           `&status=inquiry-rejected` +
@@ -60,7 +61,6 @@ export default function InquiryRejected({ session }) {
           `&manufacturer_part_number=${manufacturerPartNumberParam}` +
           `&order_date=${orderDateParam}` +
           `&action_required=${true}`
-
     await axios
       .get(isActionRequired, {
         headers: {
@@ -111,6 +111,10 @@ export default function InquiryRejected({ session }) {
     loadData()
   }, [])
 
+  useEffect(() => {
+    handleSearchData()
+  }, [stateActionRequired])
+
   return (
     <>
       <div className="mb-10">
@@ -148,17 +152,21 @@ export default function InquiryRejected({ session }) {
           </div>
           <div className="grid grid-cols-2 gap-3 mt-6">
             <div className="text-center items-center flex space-x-2">
-              <input
-                type="checkbox"
-                checked={stateActionRequired}
-                id="stateActionRequired"
-                onChange={(e) => setStateActionRequired(!stateActionRequired)}
-              />
-              <label
-                htmlFor="stateActionRequired"
-                className="ml-2 text-sm font-medium text-gray-900"
-              >
-                Action Require
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  checked={stateActionRequired}
+                  className="sr-only peer"
+                  id="stateActionRequired"
+                  onChange={(e) => {
+                    setStateActionRequired(!stateActionRequired)
+                    // handleSearchData()
+                  }}
+                />
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Action Required{' '}
+                </span>
               </label>
             </div>
           </div>
