f9023a44407067c55fdb018c7c808e5b691f5ab1
Kadek Cahya <kdkchy@venatronics.com>
2023-12-15 12:04:31 +0800

    out of stock

--- pages/admin/member/seller/product/out-of-stock.js
+++ pages/admin/member/seller/product/out-of-stock.js
@@ -33,7 +33,7 @@ export default function OutofStock({ session }) {
     setIsLoading(true)
     await axios
       .get(
-        `/seller/product/list?stock_country=${countryParam}&manufacturer_part_number=${manufacturerPartNumberParam}&sort_by=updated_at&sort_type=Desc`,
+        `/seller/product/list?out_of_stock=true&stock_country=${countryParam}&manufacturer_part_number=${manufacturerPartNumberParam}&sort_by=updated_at&sort_type=Desc`,
         {
           headers: {
             Authorization: `Bearer ${session?.accessToken}`,
