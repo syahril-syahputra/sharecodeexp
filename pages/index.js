4744c1b0a64cd2e49e58afe0e1ad34f6b3923fed
adly.ms <adly.ms@gmail.com>
2024-01-19 09:45:23 +0700

    [development]

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { PageSEO } from '@/components/Utils/SEO'
+import {useRouter} from 'next/router'
+import {PageSEO} from '@/components/Utils/SEO'
 import siteMetadata from '@/utils/siteMetadata'
 import axios from 'lib/axios'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
+import {motion} from 'framer-motion'
 import {
   fadeIn,
   staggerContainer,
@@ -17,10 +17,10 @@ import Footer from 'components/Footers/Footer.js'
 import PrimaryButton from '@/components/Interface/Buttons/PrimaryButton'
 import TextInput from '@/components/Interface/Form/TextInput'
 import Navbar from '@/components/Navbar2V'
-import { TypingText } from '@/components/TypeText'
-import { TitleText } from '@/components/TitleText'
-import { toastOptions } from '@/lib/toastOptions'
-import { toast } from 'react-toastify'
+import {TypingText} from '@/components/TypeText'
+import {TitleText} from '@/components/TitleText'
+import {toastOptions} from '@/lib/toastOptions'
+import {toast} from 'react-toastify'
 import TextInputSearchComponent from '@/components/Interface/Form/TextInputForSearchComponent'
 import Container from '@/components/Container'
 
@@ -29,14 +29,13 @@ export default function Index() {
   const [search, setSearch] = useState('')
   const [loadingSearch, setLoadingSearch] = useState(false)
   function searchComponent(event) {
-    setLoadingSearch(true)
     if (event.key === 'Enter' && !!search) {
       event.preventDefault()
       router.replace(`/product/search?q=${search}`)
     }
   }
 
-  function searchComponentOnClick(){
+  function searchComponentOnClick() {
     router.replace(`/product/search?q=${search}`)
   }
 
@@ -44,7 +43,7 @@ export default function Index() {
   const [suggestion, setSuggestion] = useState([])
   const [isSuggestionLoading, setSuggestionLoading] = useState(false)
   useEffect(() => {
-    if (!!search) {
+    if (search) {
       setSuggestionLoading(true)
       const getData = setTimeout(() => {
         axios
@@ -57,7 +56,6 @@ export default function Index() {
 
       return () => clearTimeout(getData)
     }
-    setSuggestionLoading(false)
   }, [search])
 
   const [statisticCounter, setStatisticCounter] = useState({
@@ -83,7 +81,7 @@ export default function Index() {
   const handleChange = (e) => {
     const name = e.target.name
     const value = e.target.value
-    setDataContact({ ...dataContact, [name]: value })
+    setDataContact({...dataContact, [name]: value})
   }
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -92,17 +90,19 @@ export default function Index() {
     setIsLoading(true)
     await axios
       .post('/contact-us', dataContact)
-      .then((result) => {
-        toast.success(result.data.message, toastOptions)
-        setDataContact({ subject: '', email: '', message: '' })
+      .then(() => {
+        toast.success('Your message has been send successfully.', toastOptions)
+        setDataContact({subject: '', email: '', message: ''})
       })
       .catch((error) => {
-        toast.error(error.data.message, error.message)
+        toast.error('Something went wrong.', error.message)
         setErrorMessage('Please fill your form correctly')
         setErrorInfo(error.data.data)
+        setDataContact({subject: '', email: '', message: ''})
       })
       .finally(() => {
         setIsLoading(false)
+        setDataContact({subject: '', email: '', message: ''})
       })
   }
 
@@ -117,7 +117,7 @@ export default function Index() {
         description={siteMetadata.description}
       />
       <Navbar />
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-center">
         <div className="w-full">
           <section className="flex flex-col items-center justify-end relative h-[90vh] sm:h-[72vh] bg-gradient-to-b from-transparent to-purple ">
             <Image
@@ -125,17 +125,18 @@ export default function Index() {
               alt="exepart.png"
               fill
               className="w-full h-full object-center object-cover -z-10"
-              sizes="90vw"
+              sizes="100vw"
               priority
             />
-            <Container className="my-auto flex items-center justify-center">
+            <Container className="flex items-center justify-center my-auto">
               <motion.div
                 variants={staggerContainer}
                 initial="hidden"
                 whileInView={'show'}
-                viewport={{ once: true, amount: 0.05 }}
+                viewport={{once: true, amount: 0.05}}
               >
-                <div className="flex flex-col items-center justify-center w-[70rem] text-center text-light">
+                {/* <div className=" w-full md:w-3/4 sm:w-3/4 lg:w-full 2xl:w-3/4 xl:w-3/4 px-2 lg:px-0 md:px-0   text-light"> */}
+                <div className="flex flex-col items-center justify-center text-center text-light">
                   <motion.h1
                     variants={textVariant(0.5)}
                     className="font-bold uppercase lg:px-[6px] md:px-0 text-[44px] sm:text-[40px] md:text-[40px] 2xl:text-[110px] lg:text-[110px]"
@@ -161,71 +162,7 @@ export default function Index() {
                     variants={fadeIn('up', 'tween', 0.2, 1)}
                     className="w-full ml-auto mr-auto xs:pb-10 xs:pt-8 "
                   >
-                    <div className="pt-2 sm:pt-0 pb-4 md:pb-1 lg:px-3 md:px-[3px]">
-                      <form className="flex items-center">
-                        <div className="relative w-full h-full">
-                            <input
-                              value={search}
-                              onChange={({target}) => setSearch(target.value)}
-                              onKeyDown={searchComponent}
-                              type="text"
-                              className="bg-gray-50 border border-gray-300 text-gray-900 placeholder:text-2xl sm:text-[12px] md:text-[14px] lg:text-[20px] text-sm focus:ring-blue-500 focus:border-blue-500 block w-full h-16 ps-10 p-5" 
-                              placeholder="Write part number / key word" required/>
-                                <button 
-                                  type="button" 
-                                  className="absolute inset-y-0 end-0 flex items-center pe-3 pr-6"
-                                  onClick={searchComponentOnClick}
-                                  >
-                                  <svg
-                                    className="w-6 h-6 text-footer-resources"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 20 20"
-                                  >
-                                  <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                                  />
-                                </svg>
-                              </button>
-                        </div>
-                    </form>           
-                      <div className="text-left py-2">
-                        {suggestion && suggestion.length > 0 && (
-                          <div>
-                            {isSuggestionLoading && (
-                              <div className="text-blueGray-500 text-lg">
-                                Suggestion :
-                                <i className="ml-2 fas fa-circle-notch fa-spin"></i>
-                              </div>
-                            )}
-                            {!isSuggestionLoading && (
-                              <div className="flex justify-start text-lg">
-                                <span className="text-blueGray-500">
-                                  Suggestion :
-                                </span>
-                                {suggestion.map((name, index) => (
-                                  <Link
-                                    key={`${name}--${index}`}
-                                    href={`/product/search?q=${name}`}
-                                    className="mx-1 underline text-blue-500 italic"
-                                  >
-                                    {name}
-                                  </Link>
-                                ))}
-                              </div>
-                            )}
-                          </div>
-                        )}
-                        {isSuggestionLoading && suggestion.length === 0 && (
-                          <i className="ml-2 fas fa-circle-notch fa-spin"></i>
-                        )}
-                      </div>                      
-                    </div>
+
                   </motion.div>
                 </div>
               </motion.div>
@@ -239,10 +176,10 @@ export default function Index() {
           >
             <motion.div
               variants={staggerContainer}
-              initial="show"
+              initial="hidden"
               whileInView={'show'}
               className="container mx-auto lg:px-4 px-6 md:px-6"
-              viewport={{ once: true, amount: 0.05 }}
+              viewport={{once: true, amount: 0.05}}
             >
               <motion.div variants={fadeIn('right', 'tween', 0.2, 1)}>
                 <TypingText title="/ HOW EXEPART OPERATES" textStyle={''} />
@@ -297,7 +234,7 @@ export default function Index() {
                       stocks listed in Exepart by directly contacting us from{' '}
                       <span className="bg-gradient-to-r from-amber-300 to-amber-400 bg-[length:0px_2px] hover:bg-[length:100%_2px] bg-left-bottom bg-no-repeat transition-[background-size] hover:text-footer-resources duration-500 ">
                         <a
-                          href="mailto:purchasing@exepart.com?"
+                          href="mailto:purchasing@exepart.com?subject=%5BYour%20Purpose!%5D&body=Hi!"
                           target="_blank"
                           rel="noopener noreferrer"
                         >
@@ -352,7 +289,7 @@ export default function Index() {
               variants={staggerContainer}
               initial="hidden"
               whileInView={'show'}
-              viewport={{ once: true, amount: 0.05 }}
+              viewport={{once: true, amount: 0.05}}
               className="container mx-auto lg:px-4 px-6 md:px-6"
             >
               <motion.div
@@ -433,7 +370,7 @@ export default function Index() {
               variants={staggerContainer}
               initial="hidden"
               whileInView={'show'}
-              viewport={{ once: true, amount: 0.05 }}
+              viewport={{once: true, amount: 0.05}}
               className="container mx-auto lg:px-4 px-6 md:px-6"
             >
               <motion.div
@@ -489,7 +426,7 @@ export default function Index() {
                           value={dataContact.subject}
                           onChange={handleChange}
                           className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-top-navbar mt-4 bg-gray-100 border   border-indigo-700 placeholder-gray-500"
-                          placeholder="Please input subject"
+                          placeholder="Please input  name"
                         />
                       </div>
                       <div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
